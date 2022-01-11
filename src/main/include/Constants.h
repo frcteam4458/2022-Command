08c60926100365bcf8f6df6ed2cf8fed6339afa6
Keebler17 <keebler17.exe@gmail.com>
2022-01-11 08:19:16 -0500

    i dont remember what i was fixing but i fixed it

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -1,4 +1,8 @@
 #include <frc/geometry/Translation2d.h>
+#include <units/length.h>
+#include <units/velocity.h>
+#include <units/acceleration.h>
+#include <units/angular_velocity.h>
 
 #pragma once
 #ifndef CONSTANTS_H
@@ -28,7 +32,8 @@ const frc::Translation2d BL{0_m, 0_m};
 const frc::Translation2d BR{0_m, 0_m};
 
 const units::meters_per_second_t MAX_SPEED{3.0f};
+const units::meters_per_second_squared_t MAX_ACCEL{1.0f};
 const units::radians_per_second_t MAX_ROT_SPEED{PI/2.0f};
 
-const units::meters_per_second_t ENCODER_DISTANCE_PER_PULSE{0.1f};
+const units::meter_t ENCODER_DISTANCE_PER_PULSE{0.1f};
 #endif
