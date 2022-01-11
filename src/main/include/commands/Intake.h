99bdd773a6a8ece6b3c890e5ac94c3026d8a39f0
Keebler17 <keebler17.exe@gmail.com>
2022-01-11 07:58:47 -0500

    implemented intake command, removed the subsystem

---
+++ src/main/include/commands/Intake.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <frc/motorcontrol/PWMSparkMax.h>
+
+#include <frc2/command/CommandBase.h>
+#include <frc2/command/CommandHelper.h>
+
+class Intake : public frc2::CommandHelper<frc2::CommandBase, Intake> {
+    public:
+        explicit Intake();
+        void Execute() override;
+        void End(bool interrupted) override;
+    private:
+        frc::PWMSparkMax intake;
+};
