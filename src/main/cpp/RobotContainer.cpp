5ef00de11aa6e4704d843ce538690f232268ef8f
Keebler17 <keebler17.exe@gmail.com>
2022-01-11 19:14:58 -0500

    added intake keybind

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -1,13 +1,19 @@
 #include "RobotContainer.h"
 
 RobotContainer::RobotContainer() : m_autoDrive(&m_drive),
-                                   m_teleop(&m_drive)
+                                   m_teleop(&m_drive),
+
+                                   leftStick{0},
+                                   rightStick{1},
+                                   secondPlayer{2},
+                                   intakeButton{&leftStick, 0}
 {
   ConfigureButtonBindings();
 }
 
 void RobotContainer::ConfigureButtonBindings()
 {
+  intakeButton.WhileHeld(m_intakeCommand);
 }
 
 frc2::Command *RobotContainer::GetAutonomousCommand()
--- src/main/include/RobotContainer.h
+++ src/main/include/RobotContainer.h
@@ -2,11 +2,14 @@
 #ifndef ROBOT_CONTAINER_H
 #define ROBOT_CONTAINER_H
 
+#include <frc2/command/button/JoystickButton.h>
 #include <frc2/command/Command.h>
 
 #include "commands/Drive.h"
 #include "commands/AutoDrive.h"
 #include "subsystems/Mecanum.h"
+
+#include "commands/Intake.h"
 
 class RobotContainer
 {
@@ -20,6 +23,14 @@ private:
   Mecanum m_drive;
   AutoDrive m_autoDrive;
   Drive m_teleop;
+
+  Intake m_intakeCommand;
+
+  frc::Joystick leftStick;
+  frc::Joystick rightStick;
+  frc::Joystick secondPlayer;
+
+  frc2::JoystickButton intakeButton;
 
   void ConfigureButtonBindings();
 };
